0ccb00bd1c741c198c04bfc685f507f919143c37
Ingar Almklov <ingara@gmail.com>
2014-08-23 10:39:53 +0200

    Refactor and cleanup

--- spotube.user.js
+++ spotube.user.js
@@ -11,7 +11,7 @@
 // @match       https://www.youtube.com/watch*
 // @run-at      document-end
 // ==/UserScript==
- 
+
 // Credits to https://gist.github.com/jashandeep-sohi/875dcf9e30cfb50b3300
 
 function build_action_span() {
@@ -34,21 +34,18 @@ function build_action_span() {
 }
 
 function build_action_panel() {
-  var div = document.createElement("div");
-  var inner_div = document.createElement("div");
-  
-  div.setAttribute("id", "action-panel-download");
-  div.setAttribute("class", "action-panel-content hid");
-  div.setAttribute("data-panel-loaded", "true");
-  div.setAttribute("style", "");
-  
-  inner_div.setAttribute("id", "spotube-description");
-  inner_div.setAttribute(
-    "class",
-    "yt-uix-expander yt-uix-expander-collapsed yt-uix-button-panel"
-  );
-  div.appendChild(inner_div);
-  return div;   
+    var div = document.createElement("div");
+    var inner_div = document.createElement("div");
+
+    div.setAttribute("id", "action-panel-download");
+    div.setAttribute("class", "action-panel-content hid");
+    div.setAttribute("data-panel-loaded", "true");
+    div.setAttribute("style", "");
+
+    inner_div.setAttribute("id", "spotube-description");
+    inner_div.setAttribute("class", "yt-uix-expander yt-uix-expander-collapsed yt-uix-button-panel");
+    div.appendChild(inner_div);
+    return div;
 }
 
 function get_spotify_items(callback) {
@@ -60,50 +57,49 @@ function get_spotify_items(callback) {
     xhr.open('get', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        var items = xhr.response.tracks.items.map(function(item) {
-            var a = document.createElement("a");
-            a.setAttribute("href", "http://open.spotify.com/track/" + item.id);
-            a.textContent = item.artists[0].name +
-                            " - " +
-                            item.name +
-                            " (" +
-                            item.album.name +
-                            ")";
-            return a;
-        });
-        callback(items, xhr.status);
-      } else {
-        var p = document.createElement("p");
-        p.textContent = "Could not get results";
-        callback([p], xhr.status);
-      }
+        var status = xhr.status;
+        if (status == 200) {
+            var items = xhr.response.tracks.items.map(function(item) {
+                return {
+                    link: "http://open.spotify.com/track/" + item.id,
+                    text: item.artists[0].name + " - " + item.name + " (" + item.album.name + ")"
+                };
+            });
+            if (items.length == 0) {
+                callback(null, "No results");
+            } else {
+                callback(items, null);
+            }
+        } else {
+            console.log("Got error " + xhr.status);
+            callback(null, "Could not get results.");
+        }
     };
     xhr.send();
 }
 
 window.addEventListener("load", function() {
+    console.log("Test");
     var action_span = build_action_span();
     document.body.querySelector("#watch7-secondary-actions")
-                 .appendChild(action_span);
+        .appendChild(action_span);
     var action_panel = build_action_panel();
     document.body.querySelector("#watch7-action-panels")
-                 .insertBefore(action_panel);
-    
-    get_spotify_items(function(items, status_code) {
-        if(status_code != 200) {
+        .insertBefore(action_panel);
+
+    get_spotify_items(function(items, error) {
+        if (error != null) {
             var p = document.createElement("p");
-            p.textContent = "Could not get results";
+            p.textContent = error;
             action_panel.appendChild(p);
-        }
-        else if(items.length == 0) {
-            var p = document.createElement("p");
-            p.textContent = "No results";
-            action_panel.appendChild(p);
-        }
-        else {
-            items.forEach(function(item) {
+        } else {
+            var links = items.map(function(item) {
+                var a = document.createElement("a");
+                a.setAttribute("href", item.link);
+                a.textContent = item.text;
+                return a;
+            });
+            links.forEach(function(item) {
                 var p = document.createElement("p");
                 p.appendChild(item);
                 action_panel.appendChild(p);
